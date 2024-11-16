fc471c9341a167058085f4ea20ff9f9a75111591
rouckay <farhadwardak2018@gmail.com>
2024-11-16 09:26:04 +0430

    Context Menu is added to Customer Page

--- app/Filament/Resources/CustomerResource/Pages/ListCustomers.php
+++ app/Filament/Resources/CustomerResource/Pages/ListCustomers.php
@@ -5,15 +5,27 @@ namespace App\Filament\Resources\CustomerResource\Pages;
 use App\Filament\Resources\CustomerResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
-
+use AymanAlhattami\FilamentContextMenu\Traits\PageHasContextMenu;
+use Filament\Actions\Action;
+use AymanAlhattami\FilamentContextMenu\ContextMenuDivider;
 class ListCustomers extends ListRecords
 {
+    use PageHasContextMenu;
     protected static string $resource = CustomerResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+    public function getContextMenuActions(): array
+    {
+        return [
+            Action::make(__('ایجاد مشتریان'))
+                ->url(CreateCustomer::getUrl(['/create'])),
+            Action::make(__('لیست'))
+                ->url(ListCustomers::getUrl(['/'])),
         ];
     }
 }
--- app/Filament/Resources/CustomerResource/Pages/ViewCustomer.php
+++ app/Filament/Resources/CustomerResource/Pages/ViewCustomer.php
@@ -3,11 +3,13 @@
 namespace App\Filament\Resources\CustomerResource\Pages;
 
 use App\Filament\Resources\CustomerResource;
+use App\Models\Customer;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
-
+use AymanAlhattami\FilamentContextMenu\Traits\PageHasContextMenu;
 class ViewCustomer extends ViewRecord
 {
+    use PageHasContextMenu;
     protected static string $resource = CustomerResource::class;
 
     protected function getHeaderActions(): array
@@ -16,4 +18,5 @@ class ViewCustomer extends ViewRecord
             Actions\EditAction::make(),
         ];
     }
+
 }
